32b71bcd1b383c12e846fe7f12b5d00e60a98b43
Felipe <felipeanf@ufmg.br>
2021-01-31 15:50:06 -0300

    New product feature

--- src/app/modules/new-product/pages/new-product.component.ts
+++ src/app/modules/new-product/pages/new-product.component.ts
@@ -1,31 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import { Console } from 'console';
-import {FormGroup, FormControl, FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/shared/services/products.service';
+import { Subscription } from 'rxjs';
+import { MeasurementUnitService } from 'src/app/shared/services/measurement-unit.service';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: 'new-product.component.html',
   styleUrls: ['new-product.component.css']
 })
-export class NewProductComponent implements OnInit {
+export class NewProductComponent implements OnInit, OnDestroy {
   public cadastroProduto = this.formBuilder.group({
-    nomeProduto: new FormControl(),
-    categoriaProduto: new FormControl(),
-    unidadeMedida: new FormControl(),
-    precoProduto: new FormControl(),
-    estoqueProduto: new FormControl()
+    nome: [null, Validators.required],
+    categoria: [null, Validators.required],
+    unidadeMedida: [null, Validators.required],
+    precoProdutoUnitario: [null, Validators.required],
+    quantidadeEstoque: [null, Validators.required]
   });;
-  public submitted = false;
+  public categories = [
+    { categoriaProduto: null }
+  ];
+  public measurementUnitOptions = [
+    { unidadeMedida: null }
+  ];
+  public saved = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  private productsGetCategoriesSub: Subscription;
+  private productsCreateSub: Subscription;
+  private measurementeGetAllSub: Subscription;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private measurementUnitService: MeasurementUnitService,
+  ) { }
  
   ngOnInit(): void {
+    this.productsGetCategoriesSub = this.productsService.getCategories().subscribe(result => {
+      if (result.data) {
+        this.categories = result.data;
+      }
+    });
+
+    this.measurementeGetAllSub = this.measurementUnitService.getAll().subscribe(result => {
+      if (result.data) {
+        this.measurementUnitOptions = result.data;
+      }
+    })
   }
 
   postData() {
-    console.log(this.cadastroProduto);
-    console.log(this.cadastroProduto.value);
+    this.productsCreateSub = this.productsService.create(this.cadastroProduto.value).subscribe(result => {
+      console.log(result);
+      if (result.data) {
+        this.cadastroProduto.reset();
+        this.saved = true;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.productsGetCategoriesSub) {
+      this.productsGetCategoriesSub.unsubscribe();
+    }
+    if (this.measurementeGetAllSub) {
+      this.measurementeGetAllSub.unsubscribe();
+    }
+    if (this.productsCreateSub) {
+      this.productsCreateSub.unsubscribe();
+    }
   }
 
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -13,7 +13,15 @@ export class ProductsService {
 
   constructor(private http: HttpClient) { }
 
-    getProductsData(): Observable<any>{
+    getProductsData(): Observable<any> {
       return this.http.get(this.productsUrl);
+    }
+
+    create(productData: any): Observable<any> {
+      return this.http.post('http://localhost:8000/product', { product: productData });
+    }
+
+    getCategories(): Observable<any> {
+      return this.http.get('http://localhost:8000/product-category');
     }
 }
